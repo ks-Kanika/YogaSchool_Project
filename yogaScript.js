0dcad90cafc877b90b6f36f3e726858304f62e9e
Kanika Sharma <159553456+ks-Kanika@users.noreply.github.com>
2024-08-10 12:41:40 +0000

    Functional yoga-website

--- yogaScript.js
+++ yogaScript.js
@@ -7,12 +7,50 @@
   "url": "https://www.yoursite.com",
   "image": "https://www.yoursite.com/images/yoga-studio.jpg"
 } */}
-document.addEventListener("DOMContentLoaded", function() {
-    var navbarToggler = document.querySelector(".navbar-toggler");
-    var navbarCollapse = document.querySelector(".navbar-collapse");
+// document.addEventListener("DOMContentLoaded", function() {
+//     var navbarToggler = document.querySelector(".navbar-toggler");
+//     var navbarCollapse = document.querySelector(".navbar-collapse");
 
-    navbarToggler.addEventListener("click", function() {
-        navbarCollapse.classList.toggle("show");
+//     navbarToggler.addEventListener("click", function() {
+//         navbarCollapse.classList.toggle("show");
+//     });
+// });
+// //
+function toggleMenu() {
+  const menuLinks = document.querySelector('.menu-links');
+  menuLinks.classList.toggle('active');
+}
+function toggleMenu() {
+  const menuLinks = document.querySelector('.menu-links');
+  const hamburgerIcon = document.querySelector('.hamburger-icon');
+
+  menuLinks.classList.toggle('active');
+  hamburgerIcon.classList.toggle('active');
+}
+/*About Section*/
+let slideIndex = 0;
+
+function changeSlide(n) {
+    let slides = document.getElementsByClassName("slide");
+    slides[slideIndex].classList.remove("active");
+    slideIndex = (slideIndex + n + slides.length) % slides.length;
+    slides[slideIndex].classList.add("active");
+}
+
+document.querySelectorAll('.slide').forEach((img) => {
+    img.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent any default action like navigating to a link
     });
 });
-//
+document.getElementById('readMoreButton').addEventListener('click', function() {
+    let content = document.getElementById('moreContent');
+    let button = document.getElementById('readMoreButton');
+    if (content.style.display === 'none') {
+        content.style.display = 'block';
+        button.textContent = 'Read Less'; // Change button text to 'Read Less'
+    } else {
+        content.style.display = 'none';
+        button.textContent = 'Read More'; // Change button text back to 'Read More'
+    }
+});
+/* About End*/
