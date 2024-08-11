d844393b96e319f70e5b89456e8f546c864a56a4
Kanika Sharma <159553456+ks-Kanika@users.noreply.github.com>
2024-08-11 15:31:01 +0000

    fully responsive site

--- yogaScript.js
+++ yogaScript.js
@@ -54,3 +54,15 @@ document.getElementById('readMoreButton').addEventListener('click', function() {
     }
 });
 /* About End*/
+/* Class Starts*/
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
